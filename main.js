8af36d2b9e8ff3142e5631d32c0a0f578ca7667b
Gideon Farrell <me@gideonfarrell.co.uk>
2014-12-29 12:17:34 +0000

    Initialise Spark and Doorbot in main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+var Spark = require('spark');
+var Doorbot = require('./app/doorbot');
+
+// Initialise Spark
+console.log('Initialising Spark API');
+Spark.login({
+    accessToken: process.env.SPARK_ACCESS_TOKEN
+});
+
+// Setup Doorbot
+console.log('Initialising Doorbot');
+var db = Doorbot.create(process.env.DOORBOT_UUID);
+db.subscribe('door:open', function() {
+    console.log('Door opened!');
+});
+db.subscribe('door:close', function() {
+    console.log('Door closed!');
+});
