ddf58ab656e2c85c3584d345b4b7531620f9df0c
Gideon Farrell <me@gideonfarrell.co.uk>
2015-01-04 14:22:55 +0000

    Fix ESLint issues

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var Spark    = require('spark');
 var Doorbot  = require('./app/doorbot');
 var Keen     = require('keen.io');
@@ -27,7 +29,7 @@ setInterval(function() {
 }, 10000); // 10 second polling
 
 // Set the volume on the soundcard to 100% (using Alsa)
-exec('amixer set PCM 100% unmute', function(err, stdout, stderr) {
+exec('amixer set PCM 100% unmute', function(err/*, stdout, stderr*/) {
     if(err) {
         console.error('main: unable to set volume - ' + err);
     }
@@ -37,14 +39,22 @@ exec('amixer set PCM 100% unmute', function(err, stdout, stderr) {
 console.log('Initialising Doorbot');
 var db = Doorbot.create(Config.get('robots').doorbot.id, {doorbell: Doorbell.sounds[Config.get('robots').doorbot.bellType]});
 db.subscribe('door:open', function() {
-    kClient.addEvent('door', {'door': 'front', 'status': 'open'}, function(err, res) {
+    var e = {
+        door:   'front',
+        status: 'open'
+    };
+    kClient.addEvent('door', e, function(err/*, res*/) {
         if(err) {
             console.error('Error transmitting event door:open');
         }
     });
 });
 db.subscribe('door:close', function() {
-    kClient.addEvent('door', {'door': 'front', 'status': 'closed'}, function(err, res) {
+    var e = {
+        door:   'front',
+        status: 'closed'
+    };
+    kClient.addEvent('door', e, function(err/*, res*/) {
         if(err) {
             console.error('Error transmitting event door:close');
         }
